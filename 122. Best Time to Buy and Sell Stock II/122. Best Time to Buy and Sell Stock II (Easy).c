d1302c678218ec14fd1684ce8f5b50ce39cc4bfa
danchang11 <cssource@livemail.tw>
2021-03-21 17:09:34 +0800

    Best Time to Buy and Sell Stock II  Solution

---
+++ 122. Best Time to Buy and Sell Stock II/122. Best Time to Buy and Sell Stock II (Easy).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int maxProfit(int* prices, int pricesSize){
+    int Profit = 0;
+    int MaxInt = 10000;
+    for(int i=0;i<pricesSize;i++)
+    {
+        if (prices[i]<MaxInt)
+        {
+            MaxInt = prices[i];
+        }
+        int CurPro = prices[i]- MaxInt;
+        if (CurPro > 0)
+        {
+            MaxInt = prices[i];
+            Profit+=CurPro;
+        }
+    }
+    return Profit;
+}
+
+
+int main()
+{
+    int nums[] = {1,2,3,4,1,8};
+    int numsSize = (int)(sizeof(nums) / sizeof(nums[0]));
+    int answer = maxProfit(nums, numsSize);
+    printf("³Ì¤j¦¬¯q(profit) %d\n",answer);
+    return 0;
+}
